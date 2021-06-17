7cd5df978372e59715a91a5c078d3ade6f5d1e49
m-r-q-w-e-r-t-y <anthonymendez@Anthonys-MBP.lan>
2021-06-16 23:18:06 -0400

    Initial push

---
+++ src/component/TodoForm.js
@@ -0,0 +1,43 @@
+import {Button, TextField} from "@material-ui/core"
+import React, { useState } from "react";
+import {v4 as uuid} from 'uuid'
+import { unmountComponentAtNode } from "react-dom";
+
+
+
+function TodoForm({ addTodo }) {
+    const [todo, setTodo] = useState({
+        id: "",
+        task: "", 
+        completed: false
+    });
+
+    function handleTaskInputChange(e) {
+        setTodo({...todo, task: e.target.value});
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if(todo.task.trim()) {
+            addTodo({...todo, id: uuid.v4});
+            
+            //rest task input
+            setTodo({...todo, task: ""});
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <TextField
+                label="Task" 
+                name = "task"
+                type = "text"
+                value = {todo.task}
+                onChange = {handleTaskInputChange}
+            />
+            <Button type="submit">submit</Button>
+        </form>
+    );
+}
+
+export default TodoForm
